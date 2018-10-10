5cbd043e654d486fa997197103652d51573cd777
PENNY-LAPTOP\Penelope <penelope.komes@gmail.com>
2018-10-09 23:07:12 -0600

    Use logical operators to make conditional statements changing writingutensil

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Truthy-and-Falsy-Assignment.js
@@ -0,0 +1,7 @@
+let tool = '';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+tool = 'marker';
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
