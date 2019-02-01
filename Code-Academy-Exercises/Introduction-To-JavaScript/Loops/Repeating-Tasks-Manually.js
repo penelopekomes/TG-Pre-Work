bd7d683a5eb3e6ede91abe726747ab0dd37876e5
PENNY-LAPTOP\Penelope <penelope.komes@gmail.com>
2019-02-01 11:22:51 -0700

    Manually log elements in an array to gain appreciation for loops

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Repeating-Tasks-Manually.js
@@ -0,0 +1,8 @@
+// Write your code below
+
+
+let vacationSpots = ['Belgium', 'New Orleans', 'Haight Ashbury']
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
