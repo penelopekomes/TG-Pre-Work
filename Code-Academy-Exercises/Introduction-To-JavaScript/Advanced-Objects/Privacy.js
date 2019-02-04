94335809caa178d42104d037895c5b218feed4cc
PENNY-LAPTOP\Penelope <penelope.komes@gmail.com>
2019-02-04 13:26:15 -0700

    Learn what privacy is regarding JavaScript and how developers note that a property should not be changed.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/Privacy.js
@@ -0,0 +1,10 @@
+const robot = {
+  _energyLevel: 100,
+  recharge(){
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+  }
+};
+
+robot._energyLevel = 'high';
+console.log(robot.recharge())
