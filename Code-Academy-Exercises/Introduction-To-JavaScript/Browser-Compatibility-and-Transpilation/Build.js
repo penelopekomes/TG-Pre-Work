01100083402ce05805ac2e49f86655c6e5ee17ca
PENNY-LAPTOP\Penelope <penelope.komes@gmail.com>
2019-02-12 21:48:23 -0700

    Open the main.js now stored in the lib directory.

--- Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Build.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Build.js
@@ -1,16 +1,10 @@
-{
-  "name": "learning-babel",
-  "version": "1.0.0",
-  "description": "Use Babel to transpile JavaScript ES6 to ES5",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1",
-    "build": "babel src -d lib"
-  },
-  "author": "",
-  "license": "ISC",
-  "devDependencies": {
-    "babel-cli": "^6.26.0",
-    "babel-preset-env": "^1.7.0"
-  }
-}
+"use strict";
+
+var pasta = "Spaghetti"; // ES5 syntax
+
+var meat = "Pancetta"; // ES6 syntax
+
+var sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+var carbonara = "You can make carbonara with " + pasta + ", " + meat + ", and a sauce made with " + sauce + ".";
